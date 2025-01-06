31860abae7355cdd6b9f6873f46f3df6b1b85e68
Giovanni Pellizzoni <pelissoni@gmail.com>
2025-01-06 22:04:49 +0100

    feat(delete): delete url command Implemented

--- UrlShrink/ViewModels/MainPageViewModel.cs
+++ UrlShrink/ViewModels/MainPageViewModel.cs
@@ -13,6 +13,7 @@ public sealed class MainPageViewModel
     public ObservableCollection<ShortenedUrl> Urls { get; set; } = [];
 
     public string Url { get; set; }
+
     public MainPageViewModel(IURLService urlService)
     {
         _urlService = urlService;
@@ -28,13 +29,28 @@ public sealed class MainPageViewModel
         }
 
         var shortenedUrl = await _urlService.CreateAShortenedURL(Url);
-        if(shortenedUrl == null)
+        if (shortenedUrl == null)
         {
             Application.Current?.MainPage?.DisplayAlert("Error", "Failed to shorten the URL", "OK");
             return;
         }
 
         // We can write the shortened URL to a cache or database
-        Urls.Add(new ShortenedUrl { ShortenedURL = shortenedUrl.tiny_url});
+        Urls.Add(new ShortenedUrl { ShortenedURL = shortenedUrl.tiny_url });
+        Url = string.Empty;
+    });
+
+    public ICommand DeleteCommand => new Command((url) =>
+    {
+        if (url is not string urlToDelete)
+        {
+            return;
+        }
+
+        var foundUrl = Urls.FirstOrDefault(x => x.ShortenedURL == urlToDelete);
+        if (foundUrl != null)
+        {
+            Urls.Remove(foundUrl);
+        }
     });
 }
